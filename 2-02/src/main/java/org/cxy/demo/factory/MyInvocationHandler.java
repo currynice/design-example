3218664803bb8f46f06f8e459b4a2171c510649c
chengxinyu-surface <694975984@qq.com>
2021-08-17 14:16:45 +0800

    抽象工厂模式---proxy 代码注释补充

--- 2-02/src/main/java/org/cxy/demo/factory/MyInvocationHandler.java
+++ 2-02/src/main/java/org/cxy/demo/factory/MyInvocationHandler.java
@@ -4,6 +4,7 @@ import org.cxy.demo.util.ClassLoaderUtils;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Description:   </br>
@@ -20,8 +21,21 @@ public class MyInvocationHandler implements InvocationHandler {
         this.cacheAdapter = cacheAdapter;
     }
 
+
+    /**
+     *
+     * @param proxy 目标对象
+     * @param method  目标方法
+     * @param args     目标参数
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        Method method2 =  ICacheAdapter.class.getMethod(method.getName(), ClassLoaderUtils.getClazzByArgs(args));
+//        Object invoke = method.invoke(cacheAdapter, args);
+
         return ICacheAdapter.class.getMethod(method.getName(), ClassLoaderUtils.getClazzByArgs(args)).invoke(cacheAdapter, args);
     }
 }
--- 2-02/src/main/java/org/cxy/demo/factory/MyProxy.java
+++ 2-02/src/main/java/org/cxy/demo/factory/MyProxy.java
@@ -13,10 +13,21 @@ import java.lang.reflect.Proxy;
 public class MyProxy {
 
 
+    /**
+     * 运行时创建一个动态代理类 及其实例对象 ,实现给定接口interfaceClass的新类（也称“动态代理类”）及其实例（对象）,代理的是接口(Interfaces)
+     * @param interfaceClass
+     * @param cacheAdapter
+     * @param <T>
+     * @return
+     * @throws Exception
+     */
     public static <T> T getProxy(Class<T> interfaceClass, ICacheAdapter cacheAdapter) throws Exception {
         InvocationHandler handler = new MyInvocationHandler(cacheAdapter);
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         Class<?>[] classes = interfaceClass.getInterfaces();
-        return (T) Proxy.newProxyInstance(classLoader, new Class[]{classes[0]}, handler);
+
+        // 类加载器 , proxyClass 的实现接口（这里是 interface org.cxy.demo.CacheService ） , InvocationHandler
+        Object proxy = Proxy.newProxyInstance(classLoader, new Class[]{classes[0]}, handler);
+        return (T)proxy;
     }
 }
