918f81e79e13cb3337da0354ba8475fbfff0ba73
cxy_mac <694975984@qq.com>
2021-08-19 23:49:39 +0800

    单例注释补充说明

--- 5-00/src/main/java/org/cxy/demo/Singleton_00.java
+++ 5-00/src/main/java/org/cxy/demo/Singleton_00.java
@@ -8,6 +8,8 @@ public class Singleton_00 {
 
     /**
      * cache 会在类初始化时 被初始化，适合仅用于被全局访问,不需要 延迟获取
+     *
+     * 静态变量是单一classLoader中唯一，因为一般不会有多个classLoader，可以说静态变量也是单例的
      */
     public static Map<String,String> cache = new ConcurrentHashMap<>();
 
